3790283f1fb05e163e1d72d7e8b18e8e6b07f3ec
house <94644378@qq.com>
2017-11-04 21:38:32 +0800

    topic

--- src/main/java/com/zzh/kafka/producer/ProducerController.java
+++ src/main/java/com/zzh/kafka/producer/ProducerController.java
@@ -1,14 +1,23 @@
 package com.zzh.kafka.producer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
 
 @RestController
 public class ProducerController {
 
+    @Autowired
+    KafkaTemplate<String, String> kafkaTemplate;
 
     @GetMapping("sendMessage")
     public void sendMessage() {
+        System.out.println("gg");
+        Random rdn = new Random();
 
+        kafkaTemplate.send("topic-1", "" + rdn.nextInt());
     }
 }
